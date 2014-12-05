a1858810bdbfa86c4956f8bcb12a52c4069894d0
Carpela <carpela@163.com>
2014-12-05 10:28:19 +0800

    可运行栈

--- kernel/thread.c
+++ kernel/thread.c
@@ -4,12 +4,21 @@
 #include <asm/system.h>
 #include <errno.h>
 
-int init_tss(int eax,long ebp,long edi,long esi,long gs,
+/*参数none: sys_make_thread返回地址*/
+int init_tss(int eax,long ebp,long edi,long esi,long gs,long none,
 		long ebx,long ecx,long edx,
 		long fs,long es,long ds,
 		long eip,long cs,long eflags,long esp,long ss)
 {
 	int i;
+	printk("eax=%p\n",ebx);
+	printk("ebx=%p\n",ebx);
+	printk("ecx=%p\n",ecx);
+	printk("edx=%p\n",edx);
+	printk("eip=%p\n",eip);
+	printk("ss=%p\n",ss);
+	printk("cs=%p\n",cs);
+	printk("esp=%p\n",esp);
 	char * p = (char*) get_free_page();
 	if (!p)
 		return -EAGAIN;
@@ -26,30 +35,58 @@ int init_tss(int eax,long ebp,long edi,long esi,long gs,
 	{
 		panic("Don't try to get more threads than 10!\n");
 	}
-	// printk("%d\n",cs);
 	printk("Thread number:%d\n",current->thread_number);
 	printk("Thread in use:%d\n",current->thread_inuse);
-
+	printk("Thread finded:%d\n",i);
 	current->tss[i].back_link = 0;
 	current->tss[i].esp0 = PAGE_SIZE + (long) p;
 	current->tss[i].ss0 = 0x10;
-	current->tss[i].eip = eip;
+	current->tss[i].eip = ebx;
 	current->tss[i].eflags = eflags;
-	current->tss[i].eax = 0;
+	current->tss[i].eax = eax;
 	current->tss[i].ecx = ecx;
 	current->tss[i].edx = edx;
 	current->tss[i].ebx = ebx;
-	current->tss[i].esp = esp;
-	current->tss[i].ebp = ebp;
+	current->tss[i].esp = ecx;
+	current->tss[i].ebp = edx;
 	current->tss[i].esi = esi;
 	current->tss[i].edi = edi;
 	current->tss[i].es = es & 0xffff;
 	current->tss[i].cs = cs & 0xffff;
-	current->tss[i].ss = ss & 0xffff;
+	current->tss[i].ss = ecx & 0xffff;
 	current->tss[i].ds = ds & 0xffff;
 	current->tss[i].fs = fs & 0xffff;
 	current->tss[i].gs = gs & 0xffff;
 	current->tss[i].ldt = current->tss[0].ldt;
 	current->tss[i].trace_bitmap = 0x80000000;
-	current->tss[i].i387 = current->tss[i].i387;
+	current->tss[i].i387 = current->tss[0].i387;
+
+	current->tss[0].back_link = 0;
+	current->tss[0].esp0 = PAGE_SIZE + (long) p;
+	current->tss[0].ss0 = 0x10;
+	current->tss[0].eip = ebx;
+	current->tss[0].eflags = eflags;
+	current->tss[0].eax = eax;
+	current->tss[0].ecx = ecx;
+	current->tss[0].edx = edx;
+	current->tss[0].ebx = ebx;
+	current->tss[0].esp = ecx;
+	current->tss[0].ebp = edx;
+	current->tss[0].esi = esi;
+	current->tss[0].edi = edi;
+	current->tss[0].es = es & 0xffff;
+	current->tss[0].cs = cs & 0xffff;
+	current->tss[0].ss = ss & 0xffff;
+	current->tss[0].ds = ds & 0xffff;
+	current->tss[0].fs = fs & 0xffff;
+	current->tss[0].gs = gs & 0xffff;
+	current->tss[0].ldt = current->tss[0].ldt;
+	current->tss[0].trace_bitmap = 0x80000000;
+	current->tss[0].i387 = current->tss[0].i387;
+	copy_process(10,edx,edi,esi,gs,none,
+		ebx,ecx,edx,
+		fs,es,ds,
+		ebx,cs,eflags,ecx,ss);
+	schedule();
+	return i;
 }
